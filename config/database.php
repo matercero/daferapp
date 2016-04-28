8390fd5a68684cffbe7367c55b8131db13cf817b
mangel.tercero <mangel.tercero@PCMANGELT.ayesa.es>
2016-04-28 17:27:39 +0200

    1 version

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'tallere6',
+		'password' => 'rQzh#740',
+		'database' => 'dafer2',
+		'encoding' => 'utf8'
+	);
+}
+?>
